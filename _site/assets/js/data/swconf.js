41f013c7a29c33ccecc00b48c523b1021936fc49
Marat Saidov <msaidov1@yandex.ru>
2025-02-08 16:02:49 +0100

    locked for prod

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1739026591',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1739026952',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };

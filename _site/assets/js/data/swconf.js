96b15fef1e6653fa70266fec72a98001fc3a4829
Marat Saidov <msaidov1@yandex.ru>
2025-02-08 16:09:13 +0100

    gemfile versions fix

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739027151',resources: [
+    cacheName: 'chirpy-1739027323',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

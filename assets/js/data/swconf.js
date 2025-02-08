f7b4bb057c3b6ef7eab9f67a4c05c8a1adc6f9e5
martysai <dev@msaidov.com>
2025-02-08 15:25:11 +0000

    Deploying to gh-pages from @ martysai/maratsaidov.github.io@6422c0d155061ec2860092b5cde6b7d1e941c7c1 🚀

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1739028310',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+

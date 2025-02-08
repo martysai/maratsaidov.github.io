6422c0d155061ec2860092b5cde6b7d1e941c7c1
Marat Saidov <msaidov1@yandex.ru>
2025-02-08 16:24:51 +0100

    deploy job

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739027323',resources: [
+    cacheName: 'chirpy-1739028276',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

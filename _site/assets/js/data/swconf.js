ffc8d8ba61fe65d7af43322936e3964f2214577f
Marat Saidov <dev@msaidov.com>
2025-02-16 15:19:56 +0100

    added proper favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739692012',resources: [
+    cacheName: 'chirpy-1739715531',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
